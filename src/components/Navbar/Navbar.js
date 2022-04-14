44462c1a04a51d9253ac01bcd66d9648b3b73ce9
Rasujon3 <rasujon3@gmail.com>
2022-04-14 23:56:08 +0600

    custom link done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CustomLink from "../../CustomLink/CustomLink";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      <div className="container">
         <Link className="navbar-brand" to="/">
           Random User
         </Link>
@@ -22,24 +23,22 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/home">
+              <CustomLink className="nav-link" aria-current="page" to="/home">
                 Home
-              </Link>
+              </CustomLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/users">
+              <CustomLink className="nav-link" to="/users">
                 Users
-              </Link>
+              </CustomLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/login">
+              <CustomLink className="nav-link" to="/login">
                 Login
-              </Link>
+              </CustomLink>
             </li>
           </ul>
-          <span className="navbar-text">
-            Navbar text with an inline element
-          </span>
+          <span className="navbar-text">USER</span>
         </div>
       </div>
     </nav>
