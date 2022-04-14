a5a5044690aaca3ffa23499aacd8a427a55151b0
Rasujon3 <rasujon3@gmail.com>
2022-04-15 00:24:42 +0600

    custom hook done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
+import useUsers from "../../Hook/useUsers";
 
 const Home = () => {
+  const data = useUsers("https://jsonplaceholder.typicode.com/users");
+  console.log(data);
   return (
     <div>
       <h2>Home</h2>
